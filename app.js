b2e18457dc0230cbfdc4712796a0d1fa3c37b0fd
Mxnsoon <nigga.guy@yandex.ru>
2021-05-14 21:53:19 +0300

    Cors

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors());
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -24,31 +25,6 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
-});
-
-const allowedCors = [
-  'http://mxnsoon.movies.nomoredomains.icu',
-  'https://mxnsoon.movies.nomoredomains.icu',
-  'https://www.mxnsoon.movies.nomoredomains.icu',
-  'https://api.mxnsoon.movies.nomoredomains.club',
-  'http://api.mxnsoon.movies.nomoredomains.club',
-  'https://www.api.mxnsoon.movies.nomoredomains.club',
-  'https://localhost:3000',
-  'http://localhost:3000'
-];
-
-app.use(cors({
-  origin: allowedCors,
-}));
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  next();
 });
 
 app.use(limiter);
